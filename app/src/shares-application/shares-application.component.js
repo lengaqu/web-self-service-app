c84df4d86e6322541f909170ff15c4a7a2d4fd85
codeitownit <gachuieve32@gmail.com>
2025-03-20 19:05:47 +0300

    new shares application bug fixes

--- app/src/shares-application/shares-application.component.js
+++ app/src/shares-application/shares-application.component.js
@@ -9,21 +9,82 @@
      * @description
      * Controls Application for Shares
      */
-    function SharesApplicationCtrl($scope) {
+    function SharesApplicationCtrl($scope, $filter, $mdToast, AccountService, SharesApplicationService) {
         var vm = this;
 
+        vm.form = {
+            locale: 'en',
+            dateFormat: 'dd MMMM yyyy'
+        };
+        vm.template = {};
+        vm.clientId = null;
+        vm.savingsAccounts = {};
+
+        vm.init = init;
+        vm.getSharesTemplate = getSharesTemplate;
+        vm.getAccounts = getAccounts;
         vm.clearForm = clearForm;
         vm.submit = submit;
-        vm.form = {};
+        
+        init();
 
+        function init() {
+            AccountService.getClientId().then(function(clientId) {
+                vm.clientId = clientId;
+                getAccounts(clientId);
+                getSharesTemplate(clientId, null); 
+            })
+        }
+        function getAccounts(accountNo) {
+            AccountService.getAllAccounts(accountNo).get().$promise.then(function (res) {
+                vm.savingsAccounts = res.savingsAccounts.filter(account => account.status.value === "Active");
+            });            
+        }
+
+        function getSharesTemplate(clientId, productId) {
+            SharesApplicationService.template().get({
+                clientId: clientId,
+                productId: productId
+            }).$promise.then(function(template) {
+                vm.template = template;
+                vm.form.requestedShares = vm.template.productOptions.map(product => product.totalShares)[0];
+                vm.currency = vm.template.productOptions.map(product => product.currency.name)[0];
+                vm.form.submittedDate = $filter('date','dd MMMM yyyy')(new Date(), 'dd MMMM yyyy');
+            })
+        }
         function clearForm() {
             $scope.shareApplicationForm.$setPristine();
             $scope.shareApplicationForm.$setUntouched();
-            vm.form = {};
+            vm.template = {};
+            vm.form = {
+                locale: 'en',
+                dateFormat: 'dd MMMM yyyy'
+            };
+            init();
         }
 
         function submit() {
-
+            vm.form.applicationDate = $filter('date')(new Date(), 'dd MMMM yyyy')
+            var sharesTemplate = {
+                clientId: vm.clientId,
+            };
+            var data = Object.assign({}, sharesTemplate, vm.form);
+            SharesApplicationService.shares().save(data).$promise.then(function() {
+                clearForm();
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content("Shares Application Submitted Successfully")
+                        .hideDelay(2000)
+                        .position('top right')
+                );
+            }, function(){
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content("Error Creating Shares Application")
+                        .hideDelay(2000)
+                        .position('top right')
+                );
+            });
         }
 
     }
